d2b2df163bfeaa4d14711ce8ae4b0e235a310467
moyashiki <moyashiki@gmail.com>
2015-11-08 17:26:17 +0900

    Add gemfile 'faraday' and write initialize code.

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 require 'hue'
+require 'faraday'
+
+WEATHER_URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
 
 client = Hue::Client.new
+
+connection = Faraday.new(:url => WEATHER_URL ) do |faraday|
+	faraday.request :url_encoded
+	faraday.response :logger 
+  faraday.adapter Faraday.default_adapter
+end
+

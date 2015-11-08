662754a31c54b7d904bd48f14ea6e1db62a92339
Tomoya Yashiki <moyashiki@gmail.com>
2015-11-08 17:55:01 +0900

    天気取得できた

--- main.rb
+++ main.rb
@@ -1,13 +1,21 @@
 require 'hue'
 require 'faraday'
+require 'json'
+require 'pp'
 
 WEATHER_URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
 
-client = Hue::Client.new
+#client = Hue::Client.new
+#
+#connection = Faraday.new(:url => WEATHER_URL ) do |faraday|
+#	faraday.request :url_encoded
+#	faraday.response :logger 
+#  faraday.adapter Faraday.default_adapter
+#end
+#
+#response = connection.get
 
-connection = Faraday.new(:url => WEATHER_URL ) do |faraday|
-	faraday.request :url_encoded
-	faraday.response :logger 
-  faraday.adapter Faraday.default_adapter
-end
-
+res = Faraday.get WEATHER_URL
+json_body = JSON.load(res.body)
+today_telop = json_body["forecasts"].first["telop"]
+pp today_telop
